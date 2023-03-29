1132b6a92605a598a7380b446b6359ac8ac78e56
Lsenn1 <lsenn@lion.lmu.edu>
2023-03-28 22:41:24 -0700

    added walkthrough links to the readme, updated some of the routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -32,57 +32,32 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new tag
-  Tag.create(req.body)
-    .then((tag) => {
-      if (req.body.productIds.length) {
-        const productTagIdArr = req.body.productIds.map((product_id) => {
-          return {
-            tag_id: tag.id,
-            product_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if there's no product tags, just respond
-      res.status(200).json(tag);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  Tag.create({
+    tag_name: req.body.tag_name,
+  }).then((tag) => {
+    res.json(tag);
+  });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
-  Tag.update(req.body, {
-    where: {
-      id: req.params.id,
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
     },
-  })
-    .then((tag) => {
-      return ProductTag.findAll({ where: { tag_id: req.params.id } });
-    })
-    .then((productTags) => {
-      const productTagIds = productTags.map(({ product_id }) => product_id);
-      const newProductTags = req.body.productIds
-        .filter((product_id) => !productTagIds.includes(product_id))
-        .map((product_id) => {
-          return {
-            tag_id: req.params.id,
-            product_id,
-          };
-        });
-      const productTagsToRemove = productTags
-        .filter(({ product_id }) => !req.body.productIds.includes(product_id))
-        .map(({ id }) => id);
-
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    });
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  ).then((tag) => {
+    res.json(tag);
+  }).catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
+    
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
